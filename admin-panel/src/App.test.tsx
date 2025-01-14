94fcc3d60b7ac50451c5ea1eae2704fe8f0b666d
Thabo Tshabalala <47thabo@gmail.com>
2025-01-14 14:32:38 +0200

    refactor: update App.test.tsx

--- admin-panel/src/App.test.tsx
+++ admin-panel/src/App.test.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import App from './App';
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('renders without crashing', () => {
+    render(<App />)
+  })
+})
